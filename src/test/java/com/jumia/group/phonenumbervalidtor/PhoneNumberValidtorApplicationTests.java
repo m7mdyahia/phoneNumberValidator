f30583e9495a3d365115587c037e60442c5d22fa
Mohammad Yahia <moda.yahia@gmail.com>
2021-05-07 13:27:03 +0200

    initial commit

---
+++ src/test/java/com/jumia/group/phonenumbervalidtor/PhoneNumberValidtorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumia.group.phonenumbervalidtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumberValidtorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
